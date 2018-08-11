944b220fdcc50f8adaf6cd867180e437c9fe5be1
Simone Basso <bassosimone@gmail.com>
2018-08-11 19:22:22 -0400

    Rewrite to use MK v0.9.0's FFI API

--- app/src/main/java/org/openobservatory/measurement_kit_android_sample/MainActivity.java
+++ app/src/main/java/org/openobservatory/measurement_kit_android_sample/MainActivity.java
@@ -4,16 +4,15 @@
 
 package org.openobservatory.measurement_kit_android_sample;
 
-import org.openobservatory.measurement_kit.android.IntentCallback;
-import org.openobservatory.measurement_kit.android.IntentRouter;
+import io.ooni.mk.Task;
+import io.ooni.mk.Event;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.openobservatory.measurement_kit.common.Version;
 import org.openobservatory.measurement_kit.android.LoadLibraryUtils;
 import org.openobservatory.measurement_kit.android.ResourceUtils;
-import org.openobservatory.measurement_kit.common.LogSeverity;
-import org.openobservatory.measurement_kit.nettests.BaseTest;
-import org.openobservatory.measurement_kit.nettests.NdtTest;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -21,14 +20,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 
+import java.util.concurrent.Executors;
+
 public class MainActivity extends AppCompatActivity {
     private final String TAG = "main-activity";
-
-    private final String ON_ENTRY_ID = "mk_on_entry";
-    private final String ON_EVENT_ID = "mk_on_event";
-    private final String ON_LOG_ID = "mk_on_log";
-    private final String ON_PROGRESS_ID = "mk_on_progress";
-    private final String ON_TEST_COMPLETE_ID = "mk_on_test_complete";
 
     private EditText entryText;
     private EditText logText;
@@ -76,86 +71,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /*
-     * onResume(): start receiving test events
-     */
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        IntentRouter.getInstance(getApplicationContext())
-            .register_handler(TAG, ON_ENTRY_ID,
-                new IntentCallback() {
-                    @Override
-                    public void callback(Intent intent) {
-                        String entry = intent.getStringExtra("entry");
-                        entryText.setText(String.format("%s\n", entry));
-                    }
-                })
-            .register_handler(TAG, ON_EVENT_ID,
-                new IntentCallback() {
-                    @Override
-                    public void callback(Intent intent) {
-                        String message = intent.getStringExtra("message");
-                        progressText.setText(String.format("%s\n", message));
-
-                    }
-                })
-            .register_handler(TAG, ON_LOG_ID,
-                new IntentCallback() {
-                    @Override
-                    public void callback(Intent intent) {
-                        String message = intent.getStringExtra("message");
-                        logText.append(message + "\n");
-                    }
-                })
-            .register_handler(TAG, ON_PROGRESS_ID,
-                new IntentCallback() {
-                    @Override
-                    public void callback(Intent intent) {
-                        double percent = intent.getDoubleExtra("percent", 0.0);
-                        String msg = intent.getStringExtra("message");
-                        logText.append(percent * 100.0 + "% " + msg + "\n");
-                    }
-                })
-            .register_handler(TAG, ON_TEST_COMPLETE_ID,
-                new IntentCallback() {
-                    @Override
-                    public void callback(Intent intent) {
-                        // allow running again
-                        menu.setGroupEnabled(R.id.ndt, true);
-                    }
-                });
-    }
-
-    /*
-     * onPause: stop receiving test events
-     */
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        IntentRouter.getInstance(getApplicationContext())
-            .unregister_handler(TAG, ON_ENTRY_ID)
-            .unregister_handler(TAG, ON_EVENT_ID)
-            .unregister_handler(TAG, ON_LOG_ID)
-            .unregister_handler(TAG, ON_PROGRESS_ID)
-            .unregister_handler(TAG, ON_TEST_COMPLETE_ID);
-    }
-
-    /*
      * onClicked:
      *     1. clear view
      *     2. set menu disabled to prevent concurrent tests
      *     3. run test
      */
 
-    private BaseTest make_test() {
-        // Technically not necessary. Here so that we see whether we
-        // can reduce all tests to a common interface.
-        return new NdtTest();
-    }
-
     public void onClicked(MenuItem item) {
+        // Prevent the user from clicking again on the button. Here in
+        // theory we have a race where the user can click multiple times
+        // very quickly and we would allow multiple tests to run.
+        menu.setGroupEnabled(R.id.ndt, false);
+
+        // Initialize the settings
+        final JSONObject settings = new JSONObject();
+        try {
+            JSONObject options = new JSONObject();
+            options.put("geoip_country_path",
+                ResourceUtils.get_geoip_path(this));
+            options.put("geoip_asn_path",
+                ResourceUtils.get_geoip_asnum_path(this));
+            options.put("no_file_report", 1);
+            settings.put("log_level", "INFO");
+            settings.put("options", options);
+            settings.put("name", "Ndt");
+        } catch (JSONException exc) {
+            Log.w(TAG, "Cannot serialize settings: " + exc.toString());
+            menu.setGroupEnabled(R.id.ndt, true);
+            return;
+        }
+
+        // Reset the UI to a known state
         EditText editText = (EditText) findViewById(R.id.progress);
         editText.setText("");
         editText = (EditText) findViewById(R.id.log);
@@ -163,45 +109,90 @@ public class MainActivity extends AppCompatActivity {
         editText = (EditText) findViewById(R.id.result);
         editText.setText("");
 
-        menu.setGroupEnabled(R.id.ndt, false);
+        // Use a thread from the factory to run the test. For such a small
+        // application AsyncTask would also be okay, most likely.
+        Thread thread = Executors.defaultThreadFactory().newThread(
+            new Runnable() {
+                @Override
+                public void run() {
+                    // Start the nettest and extract events from its queue.
+                    Task task = Task.startNettest(settings.toString());
+                    while (!task.isDone()) {
+                        Event evp = task.waitForNextEvent();
+                        if (evp == null) {
+                            Log.w(TAG, "Cannot wait for next event");
+                            break;
+                        }
+                        final String serialization = evp.serialize();
+                        if (serialization == null) {
+                            Log.w(TAG, "Cannot serialize event");
+                            break;
+                        }
+                        JSONObject event;
+                        try {
+                            event = new JSONObject(serialization);
+                        } catch (JSONException exc) {
+                            Log.w(TAG, "Cannot marshal event: " + exc.toString());
+                            break;
+                        }
+                        // Now that we've got the JSON event, process it.
+                        String key = event.optString("key");
+                        if (key.compareTo("log") == 0) {
+                            JSONObject value = event.optJSONObject("value");
+                            final String message = value.optString("message");
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    logText.append(message + "\n");
+                                }
+                            });
+                        } else if (key.compareTo("status.progress") == 0) {
+                            JSONObject value = event.optJSONObject("value");
+                            final double percentage = value.optDouble("percentage", 0.0);
+                            final String message = value.optString("message");
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    logText.append(percentage * 100.0 + "% " + message + "\n");
+                                }
+                            });
+                        } else if (key.compareTo("measurement") == 0) {
+                            JSONObject value = event.optJSONObject("value");
+                            final String json_str = value.optString("json_str");
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    entryText.setText(String.format("%s\n", json_str));
+                                }
+                            });
+                        } else if (key.compareTo("status.update.performance") == 0) {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    progressText.setText(String.format("%s\n", serialization));
+                                }
+                            });
+                        } else {
+                            Log.i(TAG, "Unhandled event: " + serialization);
+                        }
+                    }
+                    // When done, remember to re-renable running.
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            menu.setGroupEnabled(R.id.ndt, true);
+                        }
+                    });
+                }
+            }
+        );
 
-        IntentRouter router = IntentRouter.getInstance(getApplicationContext());
-
-        make_test()
-
-            // Set the level of verbosity of the test. Setting high level
-            // of verbosity (e.g. DEBUG2) MAY freeze applications.
-            .set_verbosity(LogSeverity.LOG_INFO)
-
-
-            // Set mandatory options without which MK won't work properly.
-            .set_options("geoip_country_path",
-                ResourceUtils.get_geoip_path(this))
-            .set_options("geoip_asn_path",
-                ResourceUtils.get_geoip_asnum_path(this))
-
-            // Not needed for tests that do not have input,
-            // like NDT or HttpInvalidRequestLine
-            //.set_input_filepath(path)
-
-            // If you set the following option, logs would not only routed
-            // through test listener but also written in specified file
-            //.set_error_filepath(path)
-
-            /*
-             * You SHOULD either specify a filepath where to write the JSON
-             * result (the same that is passed as listener's on_entry()), or
-             * you SHOULD tell MK not write any file report.
-             */
-            //.set_output_filepath(path)
-            .set_options("no_file_report", "1")
-
-            // Configure test to route event through router and start it
-            .on_entry(ON_ENTRY_ID, router)
-            .on_event(ON_EVENT_ID, router)
-            .on_log(ON_LOG_ID, router)
-            .on_progress(ON_PROGRESS_ID, router)
-
-            .start(ON_TEST_COMPLETE_ID, router);
+        if (thread == null) {
+            Log.w(TAG, "Cannot create background thread");
+            menu.setGroupEnabled(R.id.ndt, true);
+            return;
+        }
+        thread.setDaemon(true);
+        thread.start();
     }
 }
